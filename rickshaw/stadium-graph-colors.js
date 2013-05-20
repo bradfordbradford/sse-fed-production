f05cbd7892230a0be77fbdbf3783b3a698c33f85
Lindsey <lindseyb@wearetelegraph.com>
2013-05-20 17:31:51 -0500

    more documentation for Graphs

---
+++ rickshaw/stadium-graph-colors.js
@@ -0,0 +1,18 @@
+Rickshaw.namespace('Rickshaw.Fixtures.Color');
+
+Rickshaw.Fixtures.Color = function() {
+
+	this.schemes = {};
+
+	this.schemes.stadium = [
+		'#6f992d', // green
+		'#fe694b', // red
+		// '#e2b747', // orange
+		// '#439fbf', // blue
+		// '#e2b747', // yellow
+		// '#5a62d2', // purple
+		// '#47b3a4', // mint
+		// '#272727' // gray
+	].reverse();
+
+};
